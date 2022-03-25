955916894db17ad349402e949528f9e9a253920a
Joao Matos <joaoafonsoj2004@gmail.com>
2022-03-25 16:48:00 +0000

    Fixed compatibility bugs

--- src/networking/sockets/common/Socket.h
+++ src/networking/sockets/common/Socket.h
@@ -9,11 +9,11 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <winerror.h>
+#include <tchar.h>
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #endif
-#include <tchar.h>
 #include <iostream>
 
 namespace net
