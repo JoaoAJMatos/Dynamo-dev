d6a8488c4025bffdb79f77b7e43b6c4d885e12cb
Matos <joaoafonsoj2004@gmail.com>
2022-04-22 17:07:43 +0100

    Updated Node

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "servers/DNS/DNS_Server.h"
 #include "src/crypto/SHA256.h"
+#include "src/crypto/secp256k1/secp256k1.h"
+#include "src/networking/network/node/Node.h"
 
 int main()
 {
    /* servers::DNS_Server DNS(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10, 5);
     DNS.launch();*/
 
-    std::cout << crypto::sha256("Hello There");
+    Node* n = new Node();
 }
---
+++ src/dynamo/wallet/Wallet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matos on 4/22/2022.
+//
+
+#include "Wallet.h"
