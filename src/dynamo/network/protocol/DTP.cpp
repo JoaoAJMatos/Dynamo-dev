954e62805a032ec594c9a0a5e0a465af8ab29c05
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2022-06-10 23:48:13 +0100

    Added DTP protocol implementation

---
+++ src/dynamo/network/protocol/DTP.cpp
@@ -0,0 +1,59 @@
+#include "DTP.h"
+
+DTP::Packet::Packet(int type, std::string origin, std::string destination, int port, std::string payload)
+{
+    this->packetHeader.type = type;
+    this->packetHeader.origin = origin;
+    this->packetHeader.destination = destination;
+    this->packetHeader.port = port;
+    this->payload = payload;
+}
+
+DTP::Packet::Packet(std::string buffer)
+{
+    int pos = 0;
+    std::string delimiter = "|";
+
+    this->packetHeader.type = atoi(buffer.substr(pos, buffer.find(delimiter)).c_str());
+    pos += buffer.find(delimiter) + delimiter.length();
+
+    this->packetHeader.origin = buffer.substr(pos, buffer.find(delimiter));
+    pos += buffer.find(delimiter) + delimiter.length();
+
+    this->packetHeader.destination = buffer.substr(pos, buffer.find(delimiter));
+    pos += buffer.find(delimiter) + delimiter.length();
+
+    this->packetHeader.port = atoi(buffer.substr(pos, buffer.find(delimiter)).c_str());
+    pos += buffer.find(delimiter) + delimiter.length();
+
+    this->payload = buffer.substr(pos, buffer.length());
+}
+
+std::string DTP::Packet::buffer()
+{
+    std::stringstream ss;
+    ss << this->packetHeader.type << "|" << this->packetHeader.origin << "|" << this->packetHeader.destination << "|" << this->packetHeader.port << "|" << this->payload;
+    return ss.str();
+}
+
+void DTP::Packet::show()
+{
+    std::cout << std::endl;
+    std::cout << " ===== HEADERS =====" << std::endl;
+    std::cout << " - Type: " << this->packetHeader.type << std::endl;
+    std::cout << " - Origin: " << this->packetHeader.origin << std::endl;
+    std::cout << " - Destination: " << this->packetHeader.destination << std::endl;
+    std::cout << " - Port: " << this->packetHeader.port << std::endl << std::endl;
+    std::cout << " =====  BODY  =====" << std::endl;
+    std::cout << " - Payload: " << this->payload << std::endl << std::endl;
+}
+
+DTP::header DTP::Packet::headers()
+{
+    return this->packetHeader;
+}
+
+std::string DTP::Packet::getPayload()
+{
+    return this->payload;
+}
---
+++ src/dynamo/network/protocol/DTP.h
@@ -0,0 +1,68 @@
+/**
+ * @file dtp.h
+ * @author Joao Matos
+ * 
+ * @brief The structure of the Dynamo Transmition Protocol. 
+ * 
+ * @version 0.1
+ * @date 2022-06-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __DTP_H__
+#define __DTP_H__
+
+#include <iostream>
+
+#include "../../../../libs/msgpack11/msgpack11.hpp"
+
+namespace DTP
+{
+    struct header
+    {
+        int type;                   // The type of the packet (-1 - ACK ,0 - BLOCKCHAIN_DATA, 1 - BLOCKCHAIN_REQUEST, 2 - TRANSACTION, 3 - TRANSACTION_POOL_DATA, 4 - TRANSACTION_POOL_REQUEST)
+        std::string origin;         // IP address of the sender
+        std::string destination;    // IP address of the receiver
+        int port;                   // Port of the receiver
+    };
+
+    class Packet
+    {
+    private:
+        /* MEMBER VARIABLES */
+        header packetHeader;
+        
+        std::string payload;        // The payload of the packet
+
+        msgpack11::MsgPack packer; // The MsgPack object to serialize the packet
+
+    public:
+        /* CONSTRUCTOR */
+        Packet(int type, std::string origin, std::string destination, int port, std::string payload);
+        
+        Packet(std::string buffer);
+
+        /* PUBLIC FUNCTIONS */
+        /**
+         * @brief Gets the packet buffer to be sent over the network.
+         * 
+         * @return std::string 
+         */
+        std::string buffer();
+
+        /**
+         * @brief Prints the packet to the std output.
+         * 
+         */
+        void show();
+
+        /* GETTERS */
+        header headers();
+
+        std::string getPayload();
+    };
+}
+
+#endif // __DTP_H__
